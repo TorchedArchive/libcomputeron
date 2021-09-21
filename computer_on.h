c91b00fc3e2f83caf4bfd313e55556b741a3ca69
Will Eccles <will@eccles.dev>
2021-09-21 11:22:41 -0400

    more standards-compliant implementation
    - Fix non-standard include guard
    - Fix inline for non-GNU inlining
    - Constexpr and consteval in C++ as available

--- computer_on.h
+++ computer_on.h
@@ -1,8 +1,24 @@
-#ifndef _LIBCOMPUTERON_COMPUTER_ON_H_
-#define _LIBCOMPUTERON_COMPUTER_ON_H_
+#ifndef LIBCOMPUTERON_COMPUTER_ON_H
+#define LIBCOMPUTERON_COMPUTER_ON_H
 
-inline int computer_on() {
+#ifdef __cplusplus
+# if __cplusplus >= 202002L
+#  define LIBCOMPUTERON_INLINE inline consteval
+# elif __cplusplus >= 199711L
+#  define LIBCOMPUTERON_INLINE inline constexpr
+# else /* <= C++03 */
+#  define LIBCOMPUTERON_INLINE inline
+# endif /* __cplusplus >= 199711L */
+#else
+# ifndef __GNUC_GNU_INLINE__
+#  define LIBCOMPUTERON_INLINE inline static
+# else
+#  define LIBCOMPUTERON_INLINE inline
+# endif /* __GNUC_GNU_INLINE__ */
+#endif /* __cplusplus */
+
+LIBCOMPUTERON_INLINE int computer_on() {
 	return 1;
 }
 
-#endif // _LIBCOMPUTERON_COMPUTER_ON_H_
+#endif /* LIBCOMPUTERON_COMPUTER_ON_H */
